93d39c74c5d96500f19e2c94664a5e6b4f6bc861
leadattic <90087384+leadattic@users.noreply.github.com>
2024-09-27 17:29:46 +0200

    ee

---
+++ src/client/main.py
@@ -0,0 +1,4 @@
+import sockets, test
+
+
+sockets.main()
---
+++ src/client/test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+import asyncio
+
+from websockets.asyncio.client import connect
+
+async def hello():
+    uri = "ws://localhost:8765"
+    async with connect(uri) as websocket:
+        name = input("What's your name? ")
+
+        await websocket.send(name)
+        print(f">>> {name}")
+
+        greeting = await websocket.recv()
+        print(f"<<< {greeting}")
+
+def main():
+    asyncio.run(hello())
+
+
+if __name__ == "__main__":
+    main()
